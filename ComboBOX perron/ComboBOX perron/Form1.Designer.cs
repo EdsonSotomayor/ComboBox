3e31ae108c54a6d0fc2385914013da73189e1020
Alumno <Alumno@PC04>
2019-09-02 09:56:12 -0500

    v1

---
+++ ComboBOX perron/ComboBOX perron/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace ComboBOX_perron
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cbAlimentos = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.boton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // cbAlimentos
+            // 
+            this.cbAlimentos.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.cbAlimentos.FormattingEnabled = true;
+            this.cbAlimentos.Location = new System.Drawing.Point(44, 46);
+            this.cbAlimentos.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.cbAlimentos.Name = "cbAlimentos";
+            this.cbAlimentos.Size = new System.Drawing.Size(453, 25);
+            this.cbAlimentos.TabIndex = 0;
+            this.cbAlimentos.SelectedIndexChanged += new System.EventHandler(this.ComboBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(41, 129);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 17);
+            this.label1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(407, 129);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 17);
+            this.label2.TabIndex = 2;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.textBox1.Location = new System.Drawing.Point(44, 211);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(213, 23);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.TextChanged += new System.EventHandler(this.TextBox1_TextChanged);
+            // 
+            // boton
+            // 
+            this.boton.Location = new System.Drawing.Point(410, 204);
+            this.boton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.boton.Name = "boton";
+            this.boton.Size = new System.Drawing.Size(87, 37);
+            this.boton.TabIndex = 4;
+            this.boton.Text = "Agregar";
+            this.boton.UseVisualStyleBackColor = true;
+            this.boton.Click += new System.EventHandler(this.Boton_Click);
+            this.boton.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Boton_KeyPress);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(547, 317);
+            this.Controls.Add(this.boton);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cbAlimentos);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cbAlimentos;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button boton;
+    }
+}
+
